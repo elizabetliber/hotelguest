48f7e663645df1a0f9a4ccceb7ebdd7af638b771
Lisa <elizabet.liber@gmail.com>
2020-01-09 22:45:15 +0500

    добавила favicon

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ import Nav from '../components/nav'
 const Home = () => (
   <div>
     <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
+      <title>Guest House Coral</title>
+      <link rel="icon" href="/favicon(1).ico" />
     </Head>
 
     <Nav />
