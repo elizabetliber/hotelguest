2c7e969f35543298ce651f108182b79d6fbcdbcc
Lisa <elizabet.liber@gmail.com>
2020-01-13 19:58:39 +0500

    обновила карточку

---
+++ components/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+const Footer = () => (
+    <div>
+
+        <div className="card bg-dark ">
+            <div className="card-body">
+                <h3 className="card-title">GUEST HOUSE CORAL</h3>
+                <p className="card-text">г.Анапа, Коралловый проезд, д.19</p>
+                <p className="card-text">+ 7(996)376-19-78</p>
+                <a href="https://goo.gl/maps/3oANPpeMFu8qa7ja9">
+                    <button type="button" className="btn btn-outline-success">КАРТА</button>
+                    </a>
+            </div>
+
+        </div>
+
+        <style jsx>{` 
+        .card{
+        text-align:center;
+        color: white;
+        height: 100%;
+        width: 100%;
+        
+  `}</style>
+    </div>)
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,31 @@
 import React from 'react'
 import Head from 'next/head'
-import About from './About'
-import Footer from './Footer'
+import About from '../components/About'
+import Footer from '../components/Footer'
+
 const Home = () => (
   <div>
     <Head>
-
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootst.."
-            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"/>
-        <title>Guest House Coral</title>
-      <link rel="icon" href="/favicon(1).ico" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+              integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+              crossOrigin="anonymous"/>
+              <title>Guest House Coral</title>
+      <link rel="icon" href="/favicon(1).ico"/>
     </Head>
 
       <div className="hero">
-        <h1 className="title">{About()} </h1>
-
+          <h1 className="title"><About/></h1>
+      </div>
       <div className="row">
 
       </div>
-    </div>
-      <div className="oreo">{Footer()}</div>
+
+    <Footer/>
     <style jsx>{`
       .hero {
         width: 100%;
         color: white;
+        
       }
       .title {
         margin: 0;
@@ -34,36 +36,14 @@ const Home = () => (
       .title,
       .description {
         text-align: center;
+       
       }
       .row {
+       
         max-width: 880px;
         margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
+    }
+    
       
     `}</style>
   </div>
